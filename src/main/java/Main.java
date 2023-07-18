c3ccd4d5966fafc54001680d0c39bc4b2baf1431
David Ordás <3125580+davorpa@users.noreply.github.com>
2023-07-18 20:12:14 +0200

    Hello Maven con Apache Log4j2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+    static final Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello Maven");
+        logger.info("Hello Maven");
     }
 }
